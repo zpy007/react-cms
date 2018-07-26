92768e77b33ef2343cf0a50362d4b5d5fd737119
zpy <zpy2358@gmail.com>
2018-07-27 00:35:45 +0800

    创建了menu子项目

--- src/menu/menucontainer.js
+++ src/menu/menucontainer.js
@@ -2,11 +2,25 @@ import React,{Component} from 'react'
 import MenuItem from './menuitem';
 
 export default class MenuContainer extends Component{
-    
+    constructor(){
+        super();
+        this.state={
+            menuarray:[]
+        }
+    }
+    componentWillMount(){
+        this.setState({
+            menuarray:['客户信息','抵押物信息','贷后检查记录']
+        })
+        console.log('componentWillMount')
+    }
+
     render(){
         return (
             <div className='menu-container'>
-                <MenuItem menuitemname='目录项1'/>
+                
+                {this.state.menuarray.map((item,i)=><MenuItem menuitemname={item} key={i}/> )}
+                
             </div> 
         )
     }
