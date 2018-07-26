3a4660ce55e357d974632ebd951e55e2b1773036
zpy007 <zpy2358@gmail.com>
2018-07-26 22:43:40 +0800

    创建了menu子项目

---
+++ src/menu/menucontainer.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import MenuItem from './menuitem';
+
+export default class MenuContainer extends Component{
+    
+    render(){
+        return (
+            <div className='menu-container'>
+                <MenuItem menuitemname='目录项1'/>
+            </div>
+        )
+    }
+}
---
+++ src/menu/menuitem.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+export default class MenuItem extends Component{
+    handleMenuItemClick(){
+        console.log('menuitem clicked')
+    }
+    render(){
+        return(
+            <div className='menu-item' onClick={()=>{this.handleMenuItemClick()}}>
+                <span>{this.props.menuitemname}</span>
+            </div>
+        )
+    }
+}
