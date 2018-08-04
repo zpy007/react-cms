50c922460271c68db8a78d6709fe7b15906d456f
zpy007 <zpy2358@gmail.com>
2018-08-04 22:48:30 +0800

    解决了动态组件

--- src/tabs/clientinfotab.js
+++ src/tabs/clientinfotab.js
@@ -6,6 +6,11 @@ export default class ClientInfoTab extends Component{
         return (
             <div>
                 <span>客户信息选项卡</span>
+                <div>
+                    <div><span>【搜索框】搜索按钮</span></div>
+                    <div><span>数据表[姓名|身份证号|地址|电话|授信额度|期限|用途]</span></div>
+                    <div><span>分页</span></div>
+                </div>
             </div>
         )
     }
