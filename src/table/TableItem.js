10a9f067d961153117659e12dddbc6d3c466c69b
zpy <zpy2358@gmail.com>
2018-08-10 22:07:29 +0800

    增加了table模块

--- src/table/TableItem.js
+++ src/table/TableItem.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react'
 
 export default class TableItem extends Component {
     render() {
+        var columns = this.props.Columns;
+        var dataes = this.props.Data;
+        var tds=(dataes)=>{
+            return (
+                <td>
+                    {dataes.clientname}
+                </td>
+            );
+        }
         return (
+            
             <tr className="tableitem">
-                <td>
-                    {this.props.Data.clientname}
-                </td>
-                <td>
-                    {this.props.Data.clientid}
-                </td>
+                {console.log(tds)} 
+                
             </tr>
         )
     }
