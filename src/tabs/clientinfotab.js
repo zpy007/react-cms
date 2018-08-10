5e7bbc6c5c972e88fb7e8eda81b44d3caf920117
zpy007 <zpy2358@gmail.com>
2018-08-10 23:16:46 +0800

    增加了table模块

--- src/tabs/clientinfotab.js
+++ src/tabs/clientinfotab.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'C:/Users/zpy/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react'
+import React,{Component} from 'react'
 import TableContainer from '../table/tablecontainer';
 
 var mockjson2={
